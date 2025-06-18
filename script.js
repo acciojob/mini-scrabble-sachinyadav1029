79d9933dec6e9f21a62d7f121a96c2e886b9d4c4
Sachin Yadav <121727034+sachinyadav1029@users.noreply.github.com>
2025-06-18 23:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+let input = document.getElementById("evaluatedText");
+
+let h3 =  document.getElementById("letterCount");
+let val = 0;
+
+input.addEventListener("input", () => {
+      h3.textContent = input.value.length;
+});
+
